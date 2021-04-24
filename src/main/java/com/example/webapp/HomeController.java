68a9ccf6a86c41a42c6f85b872407951c6496ee7
Aditya Pandey <addherbs@gmail.com>
2021-04-24 03:35:16 -0500

    Using ModelAndView object to pass information to JSP

--- src/main/java/com/example/webapp/HomeController.java
+++ src/main/java/com/example/webapp/HomeController.java
@@ -3,20 +3,21 @@ package com.example.webapp;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("home")
-    public String home(String name, HttpSession session, @RequestParam("age") int myAge) {
+    public ModelAndView home(String name, HttpSession session, @RequestParam("age") int myAge) {
 
-        System.out.println("Came on to home Route " + name + " " + myAge);
-        session.setAttribute("name", name);
-        session.setAttribute("age", myAge);
-        return "home";
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("name", name);
+        mv.addObject("age", myAge);
+        mv.setViewName("home");
+
+        return mv;
     }
 }
